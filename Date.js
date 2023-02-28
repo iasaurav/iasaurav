0273459fca2cf5ae866d68811be14ff7e5afb191
iasaurav <101176678+iasaurav@users.noreply.github.com>
2023-02-28 22:30:01 +0530

    Update Date.js

--- Date.js
+++ Date.js
@@ -1,7 +1,4 @@
-<p id="deo"></p>
-<p id="demo"></p>
 
-<script>
 const d = new Date();
 let day = d.getDate();
 let year = d.getFullYear();
@@ -13,4 +10,4 @@ document.getElementById("demo").innerHTML = day+"/"+year+"/"+month;
 
 const d = new Date();
 document.getElementById("deo").innerHTML = d.toISOString().slice(0,4)+d.toISOString().slice(5,7)+d.toISOString().slice(8,10);
-</script>
+

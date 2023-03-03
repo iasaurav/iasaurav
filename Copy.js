f5db04b40ef394294636d16094baceeb75e8d601
iasaurav <101176678+iasaurav@users.noreply.github.com>
2023-03-03 22:52:59 +0530

    Update Copy.js

--- Copy.js
+++ Copy.js
@@ -1,235 +1,235 @@
 function myFunctioaa() {
-  var copyText = document.getElementById('aa);
+  var copyText = document.getElementById('aa');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioba() {
-  var copyText = document.getElementById('ba);
+  var copyText = document.getElementById('ba');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioca() {
-  var copyText = document.getElementById('ca);
+  var copyText = document.getElementById('ca');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioab() {
-  var copyText = document.getElementById('ab);
+  var copyText = document.getElementById('ab');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobb() {
-  var copyText = document.getElementById('bb);
+  var copyText = document.getElementById('bb');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiocb() {
-  var copyText = document.getElementById('cb);
+  var copyText = document.getElementById('cb');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioac() {
-  var copyText = document.getElementById('ac);
+  var copyText = document.getElementById('ac');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobc() {
-  var copyText = document.getElementById('bc);
+  var copyText = document.getElementById('bc');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiocc() {
-  var copyText = document.getElementById('cc);
+  var copyText = document.getElementById('cc');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioad() {
-  var copyText = document.getElementById('ad);
+  var copyText = document.getElementById('ad');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobd() {
-  var copyText = document.getElementById('bd);
+  var copyText = document.getElementById('bd');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiocd() {
-  var copyText = document.getElementById('cd);
+  var copyText = document.getElementById('cd');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioae() {
-  var copyText = document.getElementById('ae);
+  var copyText = document.getElementById('ae');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobe() {
-  var copyText = document.getElementById('be);
+  var copyText = document.getElementById('be');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioce() {
-  var copyText = document.getElementById('ce);
+  var copyText = document.getElementById('ce');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioaf() {
-  var copyText = document.getElementById('af);
+  var copyText = document.getElementById('af');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobf() {
-  var copyText = document.getElementById('bf);
+  var copyText = document.getElementById('bf');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiocf() {
-  var copyText = document.getElementById('cf);
+  var copyText = document.getElementById('cf');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioag() {
-  var copyText = document.getElementById('ag);
+  var copyText = document.getElementById('ag');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobg() {
-  var copyText = document.getElementById('bg);
+  var copyText = document.getElementById('bg');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiocg() {
-  var copyText = document.getElementById('cg);
+  var copyText = document.getElementById('cg');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioah() {
-  var copyText = document.getElementById('ah);
+  var copyText = document.getElementById('ah');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobh() {
-  var copyText = document.getElementById('bh);
+  var copyText = document.getElementById('bh');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioch() {
-  var copyText = document.getElementById('ch);
+  var copyText = document.getElementById('ch');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioai() {
-  var copyText = document.getElementById('ai);
+  var copyText = document.getElementById('ai');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobi() {
-  var copyText = document.getElementById('bi);
+  var copyText = document.getElementById('bi');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioci() {
-  var copyText = document.getElementById('ci);
+  var copyText = document.getElementById('ci');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioaj() {
-  var copyText = document.getElementById('aj);
+  var copyText = document.getElementById('aj');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobj() {
-  var copyText = document.getElementById('bj);
+  var copyText = document.getElementById('bj');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiocj() {
-  var copyText = document.getElementById('cj);
+  var copyText = document.getElementById('cj');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioak() {
-  var copyText = document.getElementById('ak);
+  var copyText = document.getElementById('ak');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobk() {
-  var copyText = document.getElementById('bk);
+  var copyText = document.getElementById('bk');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiock() {
-  var copyText = document.getElementById('ck);
+  var copyText = document.getElementById('ck');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioal() {
-  var copyText = document.getElementById('al);
+  var copyText = document.getElementById('al');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobl() {
-  var copyText = document.getElementById('bl);
+  var copyText = document.getElementById('bl');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiocl() {
-  var copyText = document.getElementById('cl);
+  var copyText = document.getElementById('cl');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioam() {
-  var copyText = document.getElementById('am);
+  var copyText = document.getElementById('am');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobm() {
-  var copyText = document.getElementById('bm);
+  var copyText = document.getElementById('bm');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiocm() {
-  var copyText = document.getElementById('cm);
+  var copyText = document.getElementById('cm');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioan() {
-  var copyText = document.getElementById('an);
+  var copyText = document.getElementById('an');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobn() {
-  var copyText = document.getElementById('bn);
+  var copyText = document.getElementById('bn');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiocn() {
-  var copyText = document.getElementById('cn);
+  var copyText = document.getElementById('cn');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioao() {
-  var copyText = document.getElementById('ao);
+  var copyText = document.getElementById('ao');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobo() {
-  var copyText = document.getElementById('bo);
+  var copyText = document.getElementById('bo');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioco() {
-  var copyText = document.getElementById('co);
+  var copyText = document.getElementById('co');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioap() {
-  var copyText = document.getElementById('ap);
+  var copyText = document.getElementById('ap');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobp() {
-  var copyText = document.getElementById('bp);
+  var copyText = document.getElementById('bp');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiocp() {
-  var copyText = document.getElementById('cp);
+  var copyText = document.getElementById('cp');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioaq() {
-  var copyText = document.getElementById('aq);
+  var copyText = document.getElementById('aq');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobq() {
-  var copyText = document.getElementById('bq);
+  var copyText = document.getElementById('bq');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiocq() {
-  var copyText = document.getElementById('cq);
+  var copyText = document.getElementById('cq');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioar() {
-  var copyText = document.getElementById('ar);
+  var copyText = document.getElementById('ar');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobr() {
-  var copyText = document.getElementById('br);
+  var copyText = document.getElementById('br');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiocr() {
-  var copyText = document.getElementById('cr);
+  var copyText = document.getElementById('cr');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioas() {
-  var copyText = document.getElementById('as);
+  var copyText = document.getElementById('as');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobs() {
-  var copyText = document.getElementById('bs);
+  var copyText = document.getElementById('bs');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiocs() {
-  var copyText = document.getElementById('cs);
+  var copyText = document.getElementById('cs');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioat() {
-  var copyText = document.getElementById('at);
+  var copyText = document.getElementById('at');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobt() {
-  var copyText = document.getElementById('bt);
+  var copyText = document.getElementById('bt');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioct() {
-  var copyText = document.getElementById('ct);
+  var copyText = document.getElementById('ct');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioau() {
-  var copyText = document.getElementById('au);
+  var copyText = document.getElementById('au');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobu() {
-  var copyText = document.getElementById('bu);
+  var copyText = document.getElementById('bu');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiocu() {
-  var copyText = document.getElementById('cu);
+  var copyText = document.getElementById('cu');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioav() {
-  var copyText = document.getElementById('av);
+  var copyText = document.getElementById('av');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobv() {
-  var copyText = document.getElementById('bv);
+  var copyText = document.getElementById('bv');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiocv() {
-  var copyText = document.getElementById('cv);
+  var copyText = document.getElementById('cv');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioaw() {
-  var copyText = document.getElementById('aw);
+  var copyText = document.getElementById('aw');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobw() {
-  var copyText = document.getElementById('bw);
+  var copyText = document.getElementById('bw');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiocw() {
-  var copyText = document.getElementById('cw);
+  var copyText = document.getElementById('cw');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioax() {
-  var copyText = document.getElementById('ax);
+  var copyText = document.getElementById('ax');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobx() {
-  var copyText = document.getElementById('bx);
+  var copyText = document.getElementById('bx');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiocx() {
-  var copyText = document.getElementById('cx);
+  var copyText = document.getElementById('cx');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioay() {
-  var copyText = document.getElementById('ay);
+  var copyText = document.getElementById('ay');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioby() {
-  var copyText = document.getElementById('by);
+  var copyText = document.getElementById('by');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiocy() {
-  var copyText = document.getElementById('cy);
+  var copyText = document.getElementById('cy');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctioaz() {
-  var copyText = document.getElementById('az);
+  var copyText = document.getElementById('az');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiobz() {
-  var copyText = document.getElementById('bz);
+  var copyText = document.getElementById('bz');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}function myFunctiocz() {
-  var copyText = document.getElementById('cz);
+  var copyText = document.getElementById('cz');
   copyText.select();
   copyText.setSelectionRange(0, 99999); navigator.clipboard.writeText(copyText.value);}

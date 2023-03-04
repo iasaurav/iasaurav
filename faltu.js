27e92b88a0184bbf99346eeb336a465391251365
iasaurav <101176678+iasaurav@users.noreply.github.com>
2023-03-04 23:57:44 +0530

    Update faltu.js

--- faltu.js
+++ faltu.js
@@ -30,7 +30,7 @@ function myItems(jsonString){
 
 
 $(document).ready(function(){
-  $("#myInput").on("change", function() {
+  $("#myInput").on("keyup", function() {
     var value = $(this).val().toLowerCase();
     $("#myTable tr").filter(function() {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)

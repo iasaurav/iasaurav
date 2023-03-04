c8ad47f4995ce94c8fb68d5f63ea323693649163
iasaurav <101176678+iasaurav@users.noreply.github.com>
2023-03-04 23:50:20 +0530

    Update faltu.js

--- faltu.js
+++ faltu.js
@@ -27,3 +27,13 @@ function myItems(jsonString){
 }
  
   
+
+
+$(document).ready(function(){
+  $("#myInput").on("change", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});

09d7e47cfc5192a105225cda5a5b16356c726d34
iasaurav <101176678+iasaurav@users.noreply.github.com>
2023-03-04 14:09:23 +0530

    Create details.js

---
+++ details.js
@@ -0,0 +1,30 @@
+var id='1vqLnaR5PgRkCdS4KBtrNhxUhIXoomoJGD_ui_yEDEDo';
+var gid = '20512574&single=true&range=b1:b6';
+
+
+
+var url = 'https://docs.google.com/spreadsheets/d/'+id+'/gviz/tq?tqx=out:json&tq&gid='+gid;
+fetch(url)
+  .then(response => response.text())
+  .then(data => document.getElementById("json").innerHTML=myItems(data.slice(47, -2))  
+  );
+function myItems(jsonString){
+  var json = JSON.parse(jsonString);
+  var table = ""
+ json.table.cols.forEach(colonne => table += '' + colonne.label + "")
+  table += ''
+  json.table.rows.forEach(ligne => {
+    table += ''
+    ligne.c.forEach(cellule => {
+        try{var valeur = cellule.f ? cellule.f : cellule.v}
+        catch(e){var valeur = ''}
+        table += "" + valeur + ""
+      }
+    )
+    table += ''
+    }
+  )
+  table += ''
+  return table
+}
+  

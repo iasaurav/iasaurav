d84b54745e3d0320d336d85f24762334a52f3256
iasaurav <101176678+iasaurav@users.noreply.github.com>
2023-03-04 23:28:28 +0530

    Create faltu.js

---
+++ faltu.js
@@ -0,0 +1,29 @@
+var id='1n5RSc9jnxqKr_rcfCZ1Uci90CApBl0y2BDn_WHs8W8A';
+var gid = '840925440';
+var source='AH1:AK';
+var url = 'https://docs.google.com/spreadsheets/d/'+id+'/gviz/tq?tqx=out:json&tq&gid='+gid+'&single=true&range='+source;
+fetch(url)
+  .then(response => response.text())
+  .then(data => document.getElementById("json").innerHTML=myItems(data.slice(47, -2))  
+  );
+function myItems(jsonString){
+  var json = JSON.parse(jsonString);
+  var table = "<table><thead><tr><th>Firstname</th><th>Lastname</th><th>Email</th><th></th></tr></thead><tbody id='myTable'>"
+  json.table.cols.forEach(colonne => table += '' + colonne.label + '')
+  table += ''
+  json.table.rows.forEach(ligne => {
+    table += "<tr>"
+    ligne.c.forEach(cellule => {
+        try{var valeur = cellule.f ? cellule.f : cellule.v}
+        catch(e){var valeur = ''}
+        table += "<td>" + valeur + "</td>"
+      }
+    )
+    table += "</tr>"
+    }
+  )
+  table += "</table>"
+  return table
+}
+ 
+  

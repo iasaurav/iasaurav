82af8fb81a03b0059f8c37db197accb5a4e1e138
iasaurav <101176678+iasaurav@users.noreply.github.com>
2023-03-17 21:08:25 +0530

    Update select.js

--- select.js
+++ select.js
@@ -1,3 +1,50 @@
+var select = document.getElementById("female"); 
+var options = ['Female Artist','Aastha-Gill','Akriti Kakar','Alka Yagnik','Anuradha Paudwal','Asees Kaur','Asha Bhosle','Chinmayi','Dhvani Bhanushali','Falguni Pathak','Hard Kaur','Harshdeep Kaur','Jasmine Sandlas','Jonita Gandhi','Jyotica Tangri','Kanika Kapoor','Lata Mangeshkar','Mahalakshmi Iyer','Mamta Sharma','Monali Thakur','Neeti Mohan','Neha Kakkar','Nikhita Gandhi','Palak Muchhal','Parampara Tandon','Rekha Bhardwaj','Shalmali Kholgade','Sharda Sinha','Shilpa Rao','Shirley Setia','Shreya Ghoshal','Shreya-Ghoshal','Shruti Pathak','Shweta Mohan','Shweta Pandit','Sona Mohapatra','Sonu Kakkar','Sunanda Sharma','Sunidhi Chauhan','Tulsi Kumar']; 
+
+// Optional: Clear all existing options first:
+select.innerHTML = "";
+// Populate list with options:
+for(var i = 0; i < options.length; i++) {
+    var opt = options[i];
+    select.innerHTML += "<option value=\"" + opt + "\">" + opt + "</option>";
+}
+  
+  
+  
+  var selec = document.getElementById("male"); 
+var option = ['Male','A. R. Rahman','Abhijeet Bhattacharya','Akhil','Amaal Mallik','Ami Mishra','Amit Trivedi','Ankit Tiwari','Arijit Singh','Arjun-Kanungo','Armaan Malik','Ash King','Atif-Aslam','Ayushmann Khurrana','B-Praak','Babbu Maan','Babul Supriyo','Badshah','Bappi Lahiri','Benny Dayal','Bohemia','Bpraak','Daler Mehndi','Darshan-Raval','Dhanush','Diljit Dosanjh','Gajendra Verma','Gulzaar-Chhaniwala','Gurdas Maan','Guru Randhawa','Hansraj-Raghuwanshi','Happy Raikoti','Himesh Reshammiya','Ikka','Jagjit Singh','Jassie-Gill','Javed Ali','Jeet Gannguli','Jubin Nautiyal','Kailash Kher','Kishore Kumar','KK','Kumar Sanu','Master Saleem','Meet Bros','Mika Singh','Mohammed Aziz','Mohammed Irfan','Mohammed Rafi','Mohit Chauhan','Nakash Aziz','Nitin Mukesh','Pankaj Udhas','Papon','Pritam','R. D. Burman','Raftaar','Rahat-Fateh-Ali-Khan','Rahul Vaidya','Roop Kumar Rathod','S. D. Burman','S. P. Balasubrahmanyam','Sanam','Shaan','Shabab Sabri','Shankar Mahadevan','Sharry-Mann','Sid Sriram','Sidhu Moose Wala','Sonu Nigam','Sukhwinder Singh','Suresh Wadkar','Tanishk Bagchi','Tochi Raina','Tony Kakkar','Udit Narayan','Vishal Dadlani','Vishal Mishra','Yasser Desai','Yo Yo Honey Singh'];
+// Optional: Clear all existing options first:
+selec.innerHTML = "";
+// Populate list with options:
+for(var a = 0; a < option.length; a++) {
+    var op = option[a];
+    selec.innerHTML += "<option value=\"" + op + "\">" + op + "</option>";
+}
+  
+  
+  
+  
+  
+  
+  var sele = document.getElementById("mail"); 
+var optio = ['Email','all','spam','trash']; 
+
+// Optional: Clear all existing options first:
+sele.innerHTML = "";
+// Populate list with options:
+for(var b = 0; b < option.length; b++) {
+    var oa = option[b];
+    sele.innerHTML += "<option value=\"" + oa + "\">" + oa + "</option>";
+}
+  
+
+
+
+
+
+
+
+
 $(function() {
 
     // bindsaurav change event to select
